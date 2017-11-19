e1c33b97db23873dd9556b178d6e57058d96cde5
lindseylavee <lavee18@up.edu>
2017-11-19 11:48:11 -0800

    Lindseys update

--- app/src/main/java/edu/up/cs301/slapjack/SJLocalGame.java
+++ app/src/main/java/edu/up/cs301/slapjack/SJLocalGame.java
@@ -87,7 +87,7 @@ public class SJLocalGame extends LocalGame {
 		// make a copy of the state; null out all cards except for the
 		// top card in the middle deck
 		SJState stateForPlayer = new SJState(state); // copy of state
-		stateForPlayer.nullAllButTopOf2(); // put nulls except for visible card
+
 		
 		// send the modified copy of the state to the player
 		p.sendInfo(stateForPlayer);
